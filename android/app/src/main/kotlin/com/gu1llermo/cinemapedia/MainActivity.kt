8af3ab115d0acbeee08d3ba681485aff80b1c569
Guillermo Nunez <guillermo6001@gmail.com>
2024-08-04 11:26:43 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/gu1llermo/cinemapedia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gu1llermo.cinemapedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const String path = '/';
+  static const String name = 'home-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
